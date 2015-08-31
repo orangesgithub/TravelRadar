584ab67b1eb576b0d4ec123bc581797c1dabfd55
unknown <yfan10x@YFAN10X-MOBL1.ccr.corp.intel.com>
2015-08-31 16:38:40 +0800

    fix radar fragment conflict
    Signed-off-by: unknown <yfan10x@YFAN10X-MOBL1.ccr.corp.intel.com>

--- travel/src/main/java/com/smart/travel/RadarFragment.java
+++ travel/src/main/java/com/smart/travel/RadarFragment.java
@@ -16,23 +16,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ViewFlipper;
 
 import com.smart.travel.adapter.TravelItemListViewAdapter;
 import com.smart.travel.model.TravelItem;
-import com.smart.travel.utils.Format;
+import com.smart.travel.net.AdviceLoader;
+import com.smart.travel.net.TicketLoader;
 import com.smart.travel.view.ScrollViewFlipper;
 import com.smart.travel.view.XListView;
 import com.viewpagerindicator.TabPageIndicator;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static android.R.layout.simple_list_item_1;
 
 
 public class RadarFragment extends Fragment implements View.OnClickListener, XListView.IXListViewListener {
+    private static final int MESSAGE_TICKET_LOADMORE = 1;
+    private static final int MESSAGE_TICKET_REFRESH = 2;
+    private static final int MESSAGE_ADVICE_LOADMORE = 3;
+    private static final int MESSAGE_ADVICE_REFRESH = 4;
+
+    private static final int VIEW_PAGE_TICKET = 0;
+    private static final int VIEW_PAGE_ADVICE = 1;
+
     private DrawerLayout drawerLayout;
     private ListView drawerList;
     private String[] drawerListItems;
@@ -40,78 +47,24 @@ public class RadarFragment extends Fragment implements View.OnClickListener, XLi
     private static final String TAG = "RadarFragment";
 
     private TravelItemListViewAdapter ticketItemAdapter;
-    private TravelItemListViewAdapter travelItemAdapter;
-    private TravelItemListViewAdapter overseasItemAdapter;
+    private TravelItemListViewAdapter adviceItemAdapter;
 
     private ViewPager viewPager;
 
-    private View container1;
-    private XListView listView1;
-    private View container2;
-    private XListView listView2;
-    private View container3;
-    private XListView listView3;
+    private View ticketContainer;
+    private XListView ticketListView;
+    private View adviceContainer;
+    private XListView adviceListView;
 
-    private int selectIndex = 0;
+    private int ticketCurrPage = 0;
+    private int adviceCurrPage = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ticketItemAdapter = new TravelItemListViewAdapter(getActivity());
-        List<TravelItem> listItems = new ArrayList<TravelItem>();
-        for (int i = 0; i < 15; i++) {
-            TravelItem travelItem = new TravelItem();
-            if (i % 2 == 0) {
-                travelItem.setImage("http://img1.3lian.com/img2011/w1/108/49/11.jpg");
-            } else {
-                travelItem.setImage("http://pic29.nipic.com/20130524/12724384_214225181194_2.jpg");
-            }
-            travelItem.setId(i);
-            travelItem.setUrl("http://www.163.com");
-            travelItem.setFrom("旅行雷达");
-            travelItem.setDate(Format.formatDate(new Date(), "yyyy-MM-dd"));
-            travelItem.setBrief("【国庆出行】5799元，普吉岛7天5晚自由行 - " + i);
-            listItems.add(travelItem);
-        }
-        ticketItemAdapter.addData(listItems);
-
-        travelItemAdapter = new TravelItemListViewAdapter(getActivity());
-        listItems = new ArrayList<TravelItem>();
-        for (int i = 0; i < 15; i++) {
-            TravelItem travelItem = new TravelItem();
-            if (i % 2 == 0) {
-                travelItem.setImage("http://f.hiphotos.baidu.com/exp/whcrop=160,120/sign=d2660476b48f8c54e386936d555910c4/0df431adcbef760936015b072edda3cc7cd99e24.jpg");
-            } else {
-                travelItem.setImage("http://www.66666333.com/upload/original/20139251133364.jpg");
-            }
-            travelItem.setId(i);
-            travelItem.setUrl("http://www.163.com");
-            travelItem.setFrom("旅行雷达");
-            travelItem.setDate(Format.formatDate(new Date(), "yyyy-MM-dd"));
-            travelItem.setBrief("【国庆出行】5799元，普吉岛7天5晚自由行 - " + i);
-            listItems.add(travelItem);
-        }
-        travelItemAdapter.addData(listItems);
-
-        overseasItemAdapter = new TravelItemListViewAdapter(getActivity());
-        listItems = new ArrayList<TravelItem>();
-        for (int i = 0; i < 15; i++) {
-            TravelItem travelItem = new TravelItem();
-            if (i % 2 == 0) {
-                travelItem.setImage("http://jingdian.landtu.com/uploadimg/destinations/000100160056.jpg");
-            } else {
-                travelItem.setImage("http://pic8.nipic.com/20100629/3015544_135545034988_2.jpg");
-            }
-            travelItem.setId(i);
-            travelItem.setUrl("http://www.163.com");
-            travelItem.setFrom("旅行雷达");
-            travelItem.setDate(Format.formatDate(new Date(), "yyyy-MM-dd"));
-            travelItem.setBrief("【国庆出行】5799元，普吉岛7天5晚自由行 - " + i);
-            listItems.add(travelItem);
-        }
-
-        overseasItemAdapter.addData(listItems);
+        adviceItemAdapter = new TravelItemListViewAdapter(getActivity());
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -135,13 +88,13 @@ public class RadarFragment extends Fragment implements View.OnClickListener, XLi
         viewPager = (ViewPager) content.findViewById(R.id.radar_pager);
         MyAdapter adapter = new MyAdapter();
         // Replace with your own listView
-        container1 = getActivity().getLayoutInflater().inflate(R.layout.radar_view_1, null);
-        listView1 = (XListView) container1.findViewById(R.id.radar_list_view1);
-        listView1.setAdapter(ticketItemAdapter);
-        listView1.setPullLoadEnable(true);
-        listView1.setXListViewListener(this);
+        ticketContainer = getActivity().getLayoutInflater().inflate(R.layout.radar_view_1, null);
+        ticketListView = (XListView) ticketContainer.findViewById(R.id.radar_list_view1);
+        ticketListView.setAdapter(ticketItemAdapter);
+        ticketListView.setPullLoadEnable(true);
+        ticketListView.setXListViewListener(this);
 
-        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ticketListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getActivity(), WebActivity.class);
@@ -151,49 +104,31 @@ public class RadarFragment extends Fragment implements View.OnClickListener, XLi
             }
         });
 
-        container2 = getActivity().getLayoutInflater().inflate(R.layout.radar_view_1, null);
-        listView2 = (XListView) container2.findViewById(R.id.radar_list_view1);
-        listView2.setAdapter(travelItemAdapter);
-        listView2.setPullLoadEnable(true);
-        listView2.setXListViewListener(this);
+        adviceContainer = getActivity().getLayoutInflater().inflate(R.layout.radar_view_1, null);
+        adviceListView = (XListView) adviceContainer.findViewById(R.id.radar_list_view1);
+        adviceListView.setAdapter(adviceItemAdapter);
+        adviceListView.setPullLoadEnable(true);
+        adviceListView.setXListViewListener(this);
 
-        listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        adviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getActivity(), WebActivity.class);
-                TravelItem travelItem = (TravelItem) travelItemAdapter.getItem(position);
+                TravelItem travelItem = (TravelItem) adviceItemAdapter.getItem(position);
                 intent.putExtra("url", travelItem.getUrl());
                 startActivity(intent);
             }
         });
 
-        container3 = getActivity().getLayoutInflater().inflate(R.layout.radar_view_1, null);
-        listView3 = (XListView) container3.findViewById(R.id.radar_list_view1);
-        listView3.setAdapter(overseasItemAdapter);
-        listView3.setPullLoadEnable(true);
-        listView3.setXListViewListener(this);
-
-        listView3.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getActivity(), WebActivity.class);
-                TravelItem travelItem = (TravelItem) overseasItemAdapter.getItem(position);
-                intent.putExtra("url", travelItem.getUrl());
-                startActivity(intent);
-            }
-        });
-
-        adapter.add(container1);
-        adapter.add(container2);
-        adapter.add(container3);
+        adapter.add(ticketContainer);
+        adapter.add(adviceContainer);
         viewPager.setAdapter(adapter);
 
         TabPageIndicator indicator = (TabPageIndicator) content.findViewById(R.id.tab_indicator);
         indicator.setViewPager(viewPager);
 
-
-        LinearLayout flipperIndicator = (LinearLayout)content.findViewById(R.id.flipper_indicator);
-        ScrollViewFlipper imageFlipper = (ScrollViewFlipper)content.findViewById(R.id.image_flipper);
+        LinearLayout flipperIndicator = (LinearLayout) content.findViewById(R.id.flipper_indicator);
+        ScrollViewFlipper imageFlipper = (ScrollViewFlipper) content.findViewById(R.id.image_flipper);
         imageFlipper.setIndicator(flipperIndicator);
         return content;
     }
@@ -207,51 +142,21 @@ public class RadarFragment extends Fragment implements View.OnClickListener, XLi
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-                case 1:
-                    if (viewPager.getCurrentItem() == 0) {
-                        listView1.stopRefresh();
-                        listView1.setRefreshTime("刚刚");
-                    }else if (viewPager.getCurrentItem() == 1){
-                        listView2.stopRefresh();
-                        listView2.setRefreshTime("刚刚");
-                    }else if (viewPager.getCurrentItem() == 2){
-                        listView3.stopRefresh();
-                        listView3.setRefreshTime("刚刚");
-                    }
+                case MESSAGE_TICKET_REFRESH:
+                    ticketListView.stopRefresh();
+                    ticketListView.setRefreshTime("刚刚");
                     break;
-                case 2:
-                    List<TravelItem> listItems = new ArrayList<TravelItem>();
-                    for (int i = 0; i < 15; i++) {
-                        TravelItem travelItem = new TravelItem();
-                        if (i % 2 == 0) {
-                            travelItem.setImage("http://img1.3lian.com/img2011/w1/108/49/11.jpg");
-                        } else {
-                            travelItem.setImage("http://pic8.nipic.com/20100629/3015544_135545034988_2.jpg");
-                        }
-                        travelItem.setUrl("http://www.baidu.com");
-                        travelItem.setId(i);
-                        travelItem.setFrom("旅行雷达");
-                        travelItem.setDate(Format.formatDate(new Date(), "yyyy-MM-dd"));
-                        travelItem.setBrief("【国庆出行】5799元，普吉岛7天5晚自由行 - " + i);
-                        listItems.add(travelItem);
-                    }
-
-                    if (viewPager.getCurrentItem() == 0) {
-                        ticketItemAdapter.addData(listItems);
-                        ticketItemAdapter.notifyDataSetChanged();
-
-                        listView1.stopLoadMore();
-                    }else if (viewPager.getCurrentItem() == 1){
-                        travelItemAdapter.addData(listItems);
-                        travelItemAdapter.notifyDataSetChanged();
-
-                        listView2.stopLoadMore();
-                    }else if (viewPager.getCurrentItem() == 2){
-                        overseasItemAdapter.addData(listItems);
-                        overseasItemAdapter.notifyDataSetChanged();
-
-                        listView3.stopLoadMore();
-                    }
+                case MESSAGE_ADVICE_REFRESH:
+                    adviceListView.stopRefresh();
+                    adviceListView.setRefreshTime("刚刚");
+                    break;
+                case MESSAGE_TICKET_LOADMORE:
+                    ticketItemAdapter.notifyDataSetChanged();
+                    ticketListView.stopLoadMore();
+                    break;
+                case MESSAGE_ADVICE_LOADMORE:
+                    adviceItemAdapter.notifyDataSetChanged();
+                    adviceListView.stopLoadMore();
                     break;
                 default:
                     break;
@@ -274,31 +179,37 @@ public class RadarFragment extends Fragment implements View.OnClickListener, XLi
         new Thread() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(1500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                if (viewPager.getCurrentItem() == VIEW_PAGE_TICKET) {
+                    handler.sendEmptyMessage(MESSAGE_TICKET_REFRESH);
+                } else if (viewPager.getCurrentItem() == VIEW_PAGE_ADVICE) {
+                    handler.sendEmptyMessage(MESSAGE_ADVICE_REFRESH);
                 }
-                handler.sendEmptyMessage(1);
-
-                Log.d(TAG, "refresh list =================");
             }
         }.start();
     }
 
     @Override
     public void onLoadMore() {
+        loadMore();
+    }
+
+    private void loadMore() {
         new Thread() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    if (viewPager.getCurrentItem() == VIEW_PAGE_TICKET) {
+                        List<TravelItem> ticketListItems = TicketLoader.load(ticketCurrPage++);
+                        ticketItemAdapter.addData(ticketListItems);
+                        handler.sendEmptyMessage(MESSAGE_TICKET_LOADMORE);
+                    } else if (viewPager.getCurrentItem() == VIEW_PAGE_ADVICE) {
+                        List<TravelItem> adviceListItems = AdviceLoader.load(adviceCurrPage++);
+                        adviceItemAdapter.addData(adviceListItems);
+                        handler.sendEmptyMessage(MESSAGE_ADVICE_LOADMORE);
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, "Radar Http Exception", e);
                 }
-                handler.sendEmptyMessage(2);
-
-                Log.d(TAG, "load more =================");
             }
         }.start();
     }
