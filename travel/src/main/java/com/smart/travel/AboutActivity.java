40b785618064835aee49c720d9ae7ebf5b1478f5
unknown <yfan10x@YFAN10X-MOBL1.ccr.corp.intel.com>
2015-09-16 23:09:46 +0800

    update about layout
    Signed-off-by: unknown <yfan10x@YFAN10X-MOBL1.ccr.corp.intel.com>

--- travel/src/main/java/com/smart/travel/AboutActivity.java
+++ travel/src/main/java/com/smart/travel/AboutActivity.java
@@ -8,15 +8,10 @@ import android.widget.TextView;
  * Created by young on 2015/9/13.
  */
 public class AboutActivity extends AppCompatActivity {
-    private TextView versionText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String version = getIntent().getStringExtra("version");
         setContentView(R.layout.activity_about);
-
-        versionText = (TextView)findViewById(R.id.about_version);
-        versionText.setText(getResources().getString(R.string.app_name) + " :" + version);
     }
 }
