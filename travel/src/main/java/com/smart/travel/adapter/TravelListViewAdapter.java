561c76ad9dce1fd6b764f1712b43c4e0f78ad179
smart <cumtsmart@gmail.com>
2015-09-22 22:19:07 +0800

    add mult list item
    Signed-off-by: smart <cumtsmart@gmail.com>

--- travel/src/main/java/com/smart/travel/adapter/TravelListViewAdapter.java
+++ travel/src/main/java/com/smart/travel/adapter/TravelListViewAdapter.java
@@ -11,29 +11,27 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.smart.travel.R;
 import com.smart.travel.model.RadarItem;
 
 public class TravelListViewAdapter extends BaseAdapter {
-    private static final String TAG = "TravelListViewAdapter";
+    private static final int TYPE_NORMAL = 0;
+    private static final int TYPE_SPECIAL = 1;
 
-    private Context context;
-    private List<RadarItem> listItems = new ArrayList<>();
+    private List<RadarItem> listItems = new ArrayList<RadarItem>();
     private LayoutInflater listContainer;
 
     public TravelListViewAdapter(Context context) {
-        this.context = context;
         this.listContainer = LayoutInflater.from(context);
-    }
-
-    public void addDataBegin(List<RadarItem> items) {
-        this.listItems.addAll(0, items);
     }
 
     public void addData(List<RadarItem> items) {
         this.listItems.addAll(items);
+    }
+
+    public void addDataBegin(List<RadarItem> items) {
+        this.listItems.addAll(0, items);
     }
 
     public List<RadarItem> getAllData() {
@@ -55,26 +53,79 @@ public class TravelListViewAdapter extends BaseAdapter {
         return 0;
     }
 
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    public int getItemViewType(int position) {
+        RadarItem item = listItems.get(position);
+        if (item.getShowType() == 3) {
+            return TYPE_SPECIAL;
+        } else {
+            return TYPE_NORMAL;
+        }
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         RadarItem item = listItems.get(position);
+        int viewType = getItemViewType(position);
 
-        if (convertView == null) {
-            convertView = listContainer.inflate(R.layout.list_travel_item, null);
+        ViewHolder viewHolder;
+        ViewHolder2 viewHolder2;
+
+        switch (viewType) {
+            case TYPE_SPECIAL:
+                if (convertView == null) {
+                    viewHolder2 = new ViewHolder2();
+                    convertView = listContainer.inflate(R.layout.list_view_item_2, null);
+                    viewHolder2.imageView = (ImageView) convertView.findViewById(R.id.imageView);
+                    viewHolder2.fromTextView = (TextView) convertView.findViewById(R.id.from);
+                    viewHolder2.dateTextView = (TextView) convertView.findViewById(R.id.date);
+                    viewHolder2.briefTextView = (TextView) convertView.findViewById(R.id.brief);
+                    convertView.setTag(viewHolder2);
+                } else {
+                    viewHolder2 = (ViewHolder2)convertView.getTag();
+                }
+
+                viewHolder2.fromTextView.setText(item.getAuthor());
+                viewHolder2.dateTextView.setText(item.getPubdate());
+                viewHolder2.briefTextView.setText(item.getTitle());
+                ImageLoader.getInstance().displayImage(item.getImage(), viewHolder2.imageView);
+                break;
+            case TYPE_NORMAL:
+                if (convertView == null) {
+                    viewHolder = new ViewHolder();
+                    convertView = listContainer.inflate(R.layout.list_view_item, null);
+                    viewHolder.imageView = (ImageView) convertView.findViewById(R.id.imageView);
+                    viewHolder.fromTextView = (TextView) convertView.findViewById(R.id.from);
+                    viewHolder.dateTextView = (TextView) convertView.findViewById(R.id.date);
+                    viewHolder.briefTextView = (TextView) convertView.findViewById(R.id.brief);
+                    convertView.setTag(viewHolder);
+                } else {
+                    viewHolder = (ViewHolder)convertView.getTag();
+                }
+
+                viewHolder.fromTextView.setText(item.getAuthor());
+                viewHolder.dateTextView.setText(item.getPubdate());
+                viewHolder.briefTextView.setText(item.getTitle());
+                ImageLoader.getInstance().displayImage(item.getImage(), viewHolder.imageView);
         }
 
-        final ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
-        TextView fromTextView = (TextView) convertView.findViewById(R.id.from);
-        TextView dateTextView = (TextView) convertView.findViewById(R.id.date);
-        TextView briefTextView = (TextView) convertView.findViewById(R.id.brief);
-
-        fromTextView.setText(item.getAuthor());
-        dateTextView.setText(item.getPubdate());
-        briefTextView.setText(item.getTitle());
-
-
-        ImageLoader.getInstance().displayImage(item.getImage(), imageView);
-
         return convertView;
+    }
+
+    static class ViewHolder {
+        ImageView imageView;
+        TextView fromTextView;
+        TextView dateTextView;
+        TextView briefTextView;
+    }
+
+    static class ViewHolder2 {
+        ImageView imageView;
+        TextView fromTextView;
+        TextView dateTextView;
+        TextView briefTextView;
     }
 }
